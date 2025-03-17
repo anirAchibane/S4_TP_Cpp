3390d97cc298c3fe3831e2f2f3405589a7b3550d
AnirAchibane <157892664+anirAchibane@users.noreply.github.com>
2025-03-17 22:39:32 +0000

    Create Movie.cpp

---
+++ TP8_Anir_Achibane/Movie.cpp
@@ -0,0 +1,38 @@
+#include "Movie.hpp"
+#include <iostream>
+
+//Définition du constructeur à paramètres
+Movie::Movie(std::string name_val, std::string rating_val, int watched_val)
+    : name(name_val), rating(rating_val), watched(watched_val) {} // Use initializer list
+
+
+//Définition des getters / setters
+void Movie::set_name(std::string newName){
+    name = newName;
+}
+
+void Movie::set_rating(std::string newRating){
+    rating = newRating;
+}
+
+std::string Movie::get_name(){
+    return name;
+}
+
+std::string Movie::get_rating(){
+    return rating;
+}
+
+int Movie::get_watched(){
+    return watched;
+}
+
+//Définition des méthodes incrementWatched(), display()
+
+void Movie::incrementWatched(){
+    watched++;
+}
+
+void Movie::display(){
+    std::cout << name << ", " << rating << ", " << watched << std::endl;
+}
