f6a9818b4d6646e0339ce0050fb6073e1cfe741e
AnirAchibane <157892664+anirAchibane@users.noreply.github.com>
2025-03-17 22:40:43 +0000

    Create main.cpp

---
+++ TP8_Anir_Achibane/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "Movie.hpp"
+#include "Movies.hpp"
+
+//Test des classes Movie et Movies et leurs méthodes:
+
+void increment_watched(Movies &movies, std::string name);
+void add_movie(Movies &movies, std::string name, std::string rating, int watched);
+
+void increment_watched(Movies &movies, std::string name) {
+    if (movies.increment_watched(name)) {
+    std::cout << name << " visionnage incrémenté" << std::endl;
+    } else {
+    std::cout << name << " introuvable" << std::endl;
+    }
+}    
+
+void add_movie(Movies &movies, std::string name, std::string rating, int watched) {
+    if (movies.add_movie(name, rating, watched)) {
+    std::cout << name << " ajouté" << std::endl;
+    } 
+    else {
+    std::cout << name << " existe déjà" << std::endl;
+    }
+}
+
+int main(){
+
+    Movies my_movies;
+
+    my_movies.display();
+
+    add_movie(my_movies, "Big", "PG-13", 2);
+    add_movie(my_movies, "Star Wars", "PG", 5);
+    add_movie(my_movies, "Cendrillon", "PG", 7);
+
+    my_movies.display();
+
+    add_movie(my_movies, "Cendrillon", "PG", 7);
+    add_movie(my_movies, "L'Âge de glace", "PG", 12);
+
+    my_movies.display();
+
+    increment_watched(my_movies, "Big");
+    increment_watched(my_movies, "L'Âge de glace");
+
+    my_movies.display();
+
+    increment_watched(my_movies, "XXX");
+
+    return 0;
+}
